22a3afa971374c4e62dca7553a26642948c4a5a2
AmosZhang200 <54987627+AmosZhang200@users.noreply.github.com>
2023-06-06 15:18:27 -0400

    Add files via upload

---
+++ writeFasta.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import glob
+import os
+
+# skip the last two rows, not the actual sequences
+path = '/Users/chuyangzhang/Downloads/20230601-Bioinformatics/'
+
+filenames = glob.glob(path+ "/*.xlsx")
+
+f = open("allSeq.fa", "w")
+
+for file in filenames:
+    df1 = pd.read_excel(file, usecols=['Name of Sequence', 'Full Promoter Sequence'], skipfooter=2)
+    df1 = df1.dropna().reset_index(drop=True)
+
+    for i in range(len(df1['Name of Sequence'])):
+        # get rid of ".xlsx" part
+        name = os.path.basename(file)[:-5]
+        name = ">" + name + " " + df1['Name of Sequence'][i] + "\n"
+        seq = df1['Full Promoter Sequence'][i] + "\n"
+        f.write(name)
+        f.write(seq)
+        f.write("\n")
+
+f.close()
